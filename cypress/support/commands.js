021b4f9bc6aa0484c8886005a043be685289ccd1
Stephen Davison <stephenjohndavison@gmail.com>
2021-08-25 17:18:20 +0100

    Increased wait time to overcome timing issues.

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -32,7 +32,7 @@
 
 // Need a wait to be imposed to allow range setting and buffering to be complete
 // due to debouncing key strokes, etc
-const WAIT_TIME = 50
+const WAIT_TIME = 100
 
 Cypress.Commands.add('arte_visit', () => {
   cy.visit('http://localhost:5501/index.html')
